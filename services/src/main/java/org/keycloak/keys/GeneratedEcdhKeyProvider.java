838213ee790385fd60884db39cfb7e21f24a110e
Justin Tay <49700559+justin-tay@users.noreply.github.com>
2023-09-29 15:37:12 +0800

    Add generated ECDH key provider

--- services/src/main/java/org/keycloak/keys/GeneratedEcdhKeyProvider.java
+++ services/src/main/java/org/keycloak/keys/GeneratedEcdhKeyProvider.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016 Red Hat, Inc. and/or its affiliates
+ * Copyright 2023 Red Hat, Inc. and/or its affiliates
  * and other contributors as indicated by the @author tags.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -19,6 +19,7 @@ package org.keycloak.keys;
 import org.jboss.logging.Logger;
 import org.keycloak.common.util.Base64;
 import org.keycloak.component.ComponentModel;
+import org.keycloak.crypto.Algorithm;
 import org.keycloak.crypto.KeyUse;
 import org.keycloak.crypto.KeyWrapper;
 import org.keycloak.models.RealmModel;
@@ -30,32 +31,39 @@ import java.security.PublicKey;
 import java.security.spec.PKCS8EncodedKeySpec;
 import java.security.spec.X509EncodedKeySpec;
 
-public class GeneratedEcdsaKeyProvider extends AbstractEcKeyProvider {
-    private static final Logger logger = Logger.getLogger(GeneratedEcdsaKeyProvider.class);
+public class GeneratedEcdhKeyProvider extends AbstractEcKeyProvider {
+    private static final Logger logger = Logger.getLogger(GeneratedEcdhKeyProvider.class);
 
-    public GeneratedEcdsaKeyProvider(RealmModel realm, ComponentModel model) {
+    public GeneratedEcdhKeyProvider(RealmModel realm, ComponentModel model) {
         super(realm, model);
     }
 
     @Override
     protected KeyWrapper loadKey(RealmModel realm, ComponentModel model) {
-        String privateEcdsaKeyBase64Encoded = model.getConfig().getFirst(GeneratedEcdsaKeyProviderFactory.ECDSA_PRIVATE_KEY_KEY);
-        String publicEcdsaKeyBase64Encoded = model.getConfig().getFirst(GeneratedEcdsaKeyProviderFactory.ECDSA_PUBLIC_KEY_KEY);
-        String ecInNistRep = model.getConfig().getFirst(GeneratedEcdsaKeyProviderFactory.ECDSA_ELLIPTIC_CURVE_KEY);
+        String privateEcdhKeyBase64Encoded = model.getConfig().getFirst(GeneratedEcdhKeyProviderFactory.ECDH_PRIVATE_KEY_KEY);
+        String publicEcdhKeyBase64Encoded = model.getConfig().getFirst(GeneratedEcdhKeyProviderFactory.ECDH_PUBLIC_KEY_KEY);
+        String ecInNistRep = model.getConfig().getFirst(GeneratedEcdhKeyProviderFactory.ECDH_ELLIPTIC_CURVE_KEY);
+        String ecdhAlgorithmMode = model.getConfig().getFirst(GeneratedEcdhKeyProviderFactory.ECDH_ALGORITHM_MODE_KEY);
 
         try {
-            PKCS8EncodedKeySpec privateKeySpec = new PKCS8EncodedKeySpec(Base64.decode(privateEcdsaKeyBase64Encoded));
+            PKCS8EncodedKeySpec privateKeySpec = new PKCS8EncodedKeySpec(Base64.decode(privateEcdhKeyBase64Encoded));
             KeyFactory kf = KeyFactory.getInstance("EC");
             PrivateKey decodedPrivateKey = kf.generatePrivate(privateKeySpec);
 
-            X509EncodedKeySpec publicKeySpec = new X509EncodedKeySpec(Base64.decode(publicEcdsaKeyBase64Encoded));
+            X509EncodedKeySpec publicKeySpec = new X509EncodedKeySpec(Base64.decode(publicEcdhKeyBase64Encoded));
             PublicKey decodedPublicKey = kf.generatePublic(publicKeySpec);
 
             KeyPair keyPair = new KeyPair(decodedPublicKey, decodedPrivateKey);
-            return createKeyWrapper(keyPair,
-                    GeneratedEcdsaKeyProviderFactory.convertECDomainParmNistRepToJWSAlgorithm(ecInNistRep), KeyUse.SIG);
+            
+            String algorithm;
+            if(GeneratedEcdhKeyProviderFactory.ECDH_ALGORITHM_MODE_DIRECT.equals(ecdhAlgorithmMode)) {
+                algorithm = Algorithm.ECDH_ES;
+            } else {
+                algorithm = GeneratedEcdhKeyProviderFactory.convertECDomainParmNistRepToJWEAlgorithm(ecInNistRep);
+            }
+            return createKeyWrapper(keyPair, algorithm, KeyUse.ENC);
         } catch (Exception e) {
-            logger.warnf("Exception at decodeEcdsaPublicKey. %s", e.toString());
+            logger.warnf("Exception at decodeEcdhPublicKey. %s", e.toString());
             return null;
         }
 
